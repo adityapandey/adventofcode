156d3f711dc364054d6df010be4c32a78666f3e9
Aditya Pandey <aditya.pandey@gmail.com>
2021-06-20 01:35:13 -0700

    Refactor day24.

--- 2018/day25/main.go
+++ 2018/day25/main.go
@@ -1,36 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/adityapandey/adventofcode/util"
 )
 
-type P [4]int
+type Pt4 [4]int
 
-func abs(i int) int {
-	if i < 0 {
-		return -i
-	}
-	return i
-}
-
-func distance(a, b P) int {
+func manhattan4(a, b Pt4) int {
 	s := 0
 	for i := 0; i < 4; i++ {
-		s += abs(a[i] - b[i])
+		s += util.Abs(a[i] - b[i])
 	}
 	return s
 }
 
-type Star struct {
-	P
-	c *Constellation
+type star struct {
+	p Pt4
+	c *constellation
 }
 
-type Constellation map[*Star]struct{}
+type constellation map[*star]struct{}
 
-func (c *Constellation) Add(s *Star) {
+func (c *constellation) add(s *star) {
 	if s.c == c {
 		return
 	}
@@ -45,30 +38,29 @@ func (c *Constellation) Add(s *Star) {
 }
 
 func main() {
-	var stars []*Star
-	var constellations []Constellation
-	s := bufio.NewScanner(os.Stdin)
+	var stars []*star
+	var constellations []constellation
+	s := util.ScanAll()
 	for s.Scan() {
-		var p P
+		var p Pt4
 		fmt.Sscanf(s.Text(), "%d,%d,%d,%d", &p[0], &p[1], &p[2], &p[3])
-		var star Star
-		star.P = p
-		m := make(Constellation)
-		m[&star] = struct{}{}
-		star.c = &m
-		stars = append(stars, &star)
+		st := star{p, nil}
+		m := make(constellation)
+		m[&st] = struct{}{}
+		st.c = &m
+		stars = append(stars, &st)
 		constellations = append(constellations, m)
 	}
 
 	for i := 0; i < len(stars)-1; i++ {
 		for j := i + 1; j < len(stars); j++ {
-			if distance(stars[i].P, stars[j].P) <= 3 {
-				stars[i].c.Add(stars[j])
+			if manhattan4(stars[i].p, stars[j].p) <= 3 {
+				stars[i].c.add(stars[j])
 			}
 		}
 	}
 
-	uniq := 0
+	var uniq int
 	for _, c := range constellations {
 		if len(c) > 0 {
 			uniq++
