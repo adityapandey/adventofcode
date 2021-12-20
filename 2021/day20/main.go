8ee46988c6f379cfc867eb3cedaf3e0a345afb82
Aditya Pandey <aditya.pandey@gmail.com>
2021-12-20 15:15:51 -0800

    map[int]byte is a string.

--- 2021/day20/main.go
+++ 2021/day20/main.go
@@ -17,11 +17,10 @@ var square = []image.Point{
 type grid struct {
 	cells        map[image.Point]byte
 	defaultValue byte
-	m            map[int]byte
 }
 
-func newGrid(m map[int]byte) grid {
-	return grid{map[image.Point]byte{}, '.', m}
+func newGrid() grid {
+	return grid{map[image.Point]byte{}, '.'}
 }
 
 func (g grid) at(p image.Point) byte {
@@ -31,7 +30,7 @@ func (g grid) at(p image.Point) byte {
 	return g.defaultValue
 }
 
-func (g *grid) enhance() {
+func (g *grid) enhance(enhancement string) {
 	next := map[image.Point]byte{}
 	var ps []image.Point
 	for p := range g.cells {
@@ -47,14 +46,14 @@ func (g *grid) enhance() {
 					sum++
 				}
 			}
-			next[image.Pt(x, y)] = g.m[sum]
+			next[image.Pt(x, y)] = enhancement[sum]
 		}
 	}
 	g.cells = next
 	if g.defaultValue == '.' {
-		g.defaultValue = g.m[0]
+		g.defaultValue = enhancement[0]
 	} else {
-		g.defaultValue = g.m[len(g.m)-1]
+		g.defaultValue = enhancement[511]
 	}
 }
 
@@ -68,29 +67,10 @@ func (g grid) litCount() int {
 	return sum
 }
 
-func (g grid) String() string {
-	var sb strings.Builder
-	var ps []image.Point
-	for p := range g.cells {
-		ps = append(ps, p)
-	}
-	b := util.Bounds(ps)
-	for y := b.Min.Y; y <= b.Max.Y; y++ {
-		for x := b.Min.X; x <= b.Max.X; x++ {
-			fmt.Fprintf(&sb, "%c", g.at(image.Pt(x, y)))
-		}
-		fmt.Fprintln(&sb)
-	}
-	return sb.String()
-}
-
 func main() {
-	m := map[int]byte{}
 	sp := strings.Split(util.ReadAll(), "\n\n")
-	for i := 0; i < len(sp[0]); i++ {
-		m[i] = sp[0][i]
-	}
-	g := newGrid(m)
+	enhancement := sp[0]
+	g := newGrid()
 	for y, line := range strings.Split(sp[1], "\n") {
 		for x := 0; x < len(line); x++ {
 			g.cells[image.Pt(x, y)] = line[x]
@@ -98,7 +78,7 @@ func main() {
 	}
 
 	for i := 1; i <= 50; i++ {
-		g.enhance()
+		g.enhance(enhancement)
 		if i == 2 || i == 50 {
 			fmt.Println(g.litCount())
 		}
