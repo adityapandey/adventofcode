fd8f25446cc9278510c8d021ec3b23c2936ad4d0
Aditya Pandey <aditya.pandey@gmail.com>
2022-12-05 22:00:23 -0800

    2022 day06.

---
+++ 2022/day06/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adityapandey/adventofcode/util"
+)
+
+func main() {
+	s := util.ReadAll()
+	fmt.Println(firstNUnique(s, 4))
+	fmt.Println(firstNUnique(s, 14))
+}
+
+func firstNUnique(s string, n int) int {
+	for i := n; i < len(s); i++ {
+		b := []byte(s[i-n : i])
+		if len(b) == len(util.SetOf(b).Values()) {
+			return i
+		}
+	}
+	return -1
+}
