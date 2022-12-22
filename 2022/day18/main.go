92dc385e85079cfae12611a5dba5027584de3cc5
Aditya Pandey <aditya.pandey@gmail.com>
2022-12-22 13:36:03 -0800

    Simplify 2022 day18.

--- 2022/day18/main.go
+++ 2022/day18/main.go
@@ -9,23 +9,30 @@ import (
 
 func main() {
 	cubes := map[util.Pt3]struct{}{}
+	neighbors := []util.Pt3{
+		{-1, 0, 0},
+		{1, 0, 0},
+		{0, -1, 0},
+		{0, 1, 0},
+		{0, 0, -1},
+		{0, 0, 1},
+	}
+	min := util.Pt3{math.MaxInt, math.MaxInt, math.MaxInt}
+	max := util.Pt3{math.MinInt, math.MinInt, math.MinInt}
 	s := util.ScanAll()
 	for s.Scan() {
 		var cube util.Pt3
 		fmt.Sscanf(s.Text(), "%d,%d,%d", &cube.X, &cube.Y, &cube.Z)
 		cubes[cube] = struct{}{}
+		min = util.Pt3{util.Min(min.X, cube.X), util.Min(min.Y, cube.Y), util.Min(min.Z, cube.Z)}
+		max = util.Pt3{util.Max(max.X, cube.X), util.Max(max.Y, cube.Y), util.Max(max.Z, cube.Z)}
 	}
+	min = min.Add(util.Pt3{-1, -1, -1})
+	max = max.Add(util.Pt3{1, 1, 1})
 
 	faces := 0
 	for cube := range cubes {
-		for _, delta := range []util.Pt3{
-			{-1, 0, 0},
-			{1, 0, 0},
-			{0, -1, 0},
-			{0, 1, 0},
-			{0, 0, -1},
-			{0, 0, 1},
-		} {
+		for _, delta := range neighbors {
 			if _, ok := cubes[cube.Add(delta)]; !ok {
 				faces++
 			}
@@ -34,31 +41,12 @@ func main() {
 	fmt.Println(faces)
 
 	faces = 0
-	min := util.Pt3{math.MaxInt, math.MaxInt, math.MaxInt}
-	max := util.Pt3{math.MinInt, math.MinInt, math.MinInt}
-	for cube := range cubes {
-		min = util.Pt3{util.Min(min.X, cube.X), util.Min(min.Y, cube.Y), util.Min(min.Z, cube.Z)}
-		max = util.Pt3{util.Max(max.X, cube.X), util.Max(max.Y, cube.Y), util.Max(max.Z, cube.Z)}
-	}
-	min = min.Add(util.Pt3{-1, -1, -1})
-	max = max.Add(util.Pt3{1, 1, 1})
-
 	q := []util.Pt3{min}
-	seen := map[util.Pt3]struct{}{}
-	for cube := range cubes {
-		seen[cube] = struct{}{}
-	}
+	seen := map[util.Pt3]struct{}{min: {}}
 	for len(q) > 0 {
 		curr := q[0]
 		q = q[1:]
-		for _, delta := range []util.Pt3{
-			{-1, 0, 0},
-			{1, 0, 0},
-			{0, -1, 0},
-			{0, 1, 0},
-			{0, 0, -1},
-			{0, 0, 1},
-		} {
+		for _, delta := range neighbors {
 			next := curr.Add(delta)
 			if next.X < min.X ||
 				next.Y < min.Y ||
@@ -70,8 +58,7 @@ func main() {
 			}
 			if _, ok := cubes[next]; ok {
 				faces++
-			}
-			if _, ok := seen[next]; !ok {
+			} else if _, ok := seen[next]; !ok {
 				seen[next] = struct{}{}
 				q = append(q, next)
 			}
