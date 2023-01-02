efa846b3e1818418877e9306a1e4e4da591c917f
Aditya Pandey <aditya.pandey@gmail.com>
2023-01-01 22:08:50 -0800

    2022 day25!

---
+++ 2022/day25/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/adityapandey/adventofcode/util"
+)
+
+func main() {
+	sum := 0
+	s := util.ScanAll()
+	for s.Scan() {
+		sum += fromSnafu(s.Text())
+	}
+	fmt.Println(toSnafu(sum))
+}
+
+func fromSnafu(s string) int {
+	var n int
+	for i := range s {
+		n *= 5
+		switch s[i] {
+		case '=':
+			n -= 2
+		case '-':
+			n--
+		default:
+			n += int(s[i]) - '0'
+		}
+	}
+	return n
+}
+
+func toSnafu(n int) string {
+	var b []byte
+	for ; n > 0; n /= 5 {
+		switch n % 5 {
+		case 3:
+			n += 5
+			b = append(b, '=')
+		case 4:
+			n += 5
+			b = append(b, '-')
+		default:
+			b = append(b, byte('0'+n%5))
+		}
+	}
+	for i := 0; i < len(b)/2; i++ {
+		b[i], b[len(b)-i-1] = b[len(b)-1-i], b[i]
+	}
+	return string(b)
+}
